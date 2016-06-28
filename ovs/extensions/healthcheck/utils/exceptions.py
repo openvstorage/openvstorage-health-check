b92312f976fa9731d285d7876e7969fc2d639f26
Jonas Libbrecht <Jonas.libbrecht@yahoo.com>
2016-06-28 15:07:22 +0200

    add custom exceptions from OVS-impl_issue_43_issue_48 (test2)

---
+++ ovs/extensions/healthcheck/utils/exceptions.py
@@ -0,0 +1,45 @@
+# Copyright (C) 2016 iNuron NV
+#
+# This file is part of Open vStorage Open Source Edition (OSE),
+# as available from
+#
+#      http://www.openvstorage.org and
+#      http://www.openvstorage.com.
+#
+# This file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License v3 (GNU AGPLv3)
+# as published by the Free Software Foundation, in version 3 as it comes
+# in the LICENSE.txt file of the Open vStorage OSE distribution.
+#
+# Open vStorage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY of any kind.
+
+"""
+Module containing the exceptions used in the Healthcheck
+"""
+
+
+class ObjectNotFoundException(Exception):
+    """
+    Raised when an object was queries that doesn't exist
+    """
+    pass
+
+
+class CommandException(Exception):
+    """
+    Raised when an object was queries that returns non-zero
+    """
+    pass
+
+class UnsupportedPlatformException(Exception):
+    """
+    Raised when an platform is not supported
+    """
+    pass
+
+class ScrubberException(Exception):
+    """
+    Raised when scrubber failed
+    """
+    pass
