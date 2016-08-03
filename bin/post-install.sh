7342bb2e1d7fd0a91bcff6f3286eb436e3d93d30
Jonas Libbrecht <Jonas.libbrecht@yahoo.com>
2016-08-03 17:17:34 +0200

    revert wrong commit

--- bin/post-install.sh
+++ bin/post-install.sh
@@ -20,7 +20,5 @@ cp ../* /opt/OpenvStorage -R
 cp ../scripts/system/ovs /usr/bin/ovs
 chmod 755 /usr/bin/ovs
 wget https://bootstrap.pypa.io/get-pip.py; python get-pip.py
-#pip install flower
 pip install psutil
-pip install xmltodict
 pip install timeout-decorator
