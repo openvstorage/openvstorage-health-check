ac47aa3cf4993a5baa1c219d03a2da41ba37deec
Jeffrey Devloo <jeffreydevloo@hotmail.com>
2017-05-16 18:17:00 +0200

    Up to speed with all latest changes

--- ovs/extensions/healthcheck/arakoon_hc.py
+++ ovs/extensions/healthcheck/arakoon_hc.py
@@ -82,7 +82,7 @@ class ArakoonHealthCheck(object):
             missing_nodes_per_cluster = {}
             missing_tlog_per_cluster = {}
 
-            tlog_dir = arakoon_config.export()[ArakoonHealthCheck.LOCAL_SR.machine_id]['tlog_dir']
+            tlog_dir = arakoon_config.export_dict()[ArakoonHealthCheck.LOCAL_SR.machine_id]['tlog_dir']
             for node_id in master_node_ids:
                 machine = StoragerouterHelper.get_by_machine_id(node_id)
                 if machine is None:
