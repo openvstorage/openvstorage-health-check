abee240a6420034b584149b63fb1bf9f896c58bb
simon <simon.vandenbossche@openvstorage.com>
2017-10-31 14:53:40 +0100

    fixed or stmt 'typo'

--- ovs/extensions/healthcheck/volumedriver_hc.py
+++ ovs/extensions/healthcheck/volumedriver_hc.py
@@ -337,7 +337,7 @@ class VolumedriverHealthCheck(object):
         :param critical_vol_number: maximal number of volumes that result in a warning
         :type critical_vol_number: int
         """
-        if not isinstance(critical_vol_number, int) and critical_vol_number >= 0:
+        if not isinstance(critical_vol_number, int) or critical_vol_number >= 0:
             raise ValueError('Critical volume number should be a positive integer')
 
         for std in VolumedriverHealthCheck.LOCAL_SR.storagedrivers:
